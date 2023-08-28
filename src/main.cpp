3aec52ad0cd6907344ec00a7ab464aecdc0c2b2f
Chadi Hamrouni <chadi.hamrouni@medtech.tn>
2023-08-28 12:37:27 +0100

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,11 @@
 #include <iostream>
 #include "../header/guioverlay.h"
 
-int Width = 1280;
+#include <cstdlib>
+#include <ctime>
+#include <random> // Include the random library
+
+int Width = 1280;  
 int Height = 720;
 
 const int NumRows = 14;
@@ -20,6 +24,7 @@ struct City
     int y;
 };
 
+
 double calculateDistance(const City& city1, const City& city2)
 {
     int dx = city2.x - city1.x;
@@ -27,86 +32,38 @@ double calculateDistance(const City& city1, const City& city2)
     return std::sqrt(dx * dx + dy * dy);
 }
 
-double calculateTourDistance(const std::vector<City>& tour)
-{
-    double totalDistance = 0.0;
-    for (std::size_t i = 0; i < tour.size() - 1; ++i)
-    {
-        totalDistance += calculateDistance(tour[i], tour[i + 1]);
+double calcdist(std::vector<City>& points) {
+   
+    double sum = 0;
+    for (int i = 0; i < points.size()-1; i++) {
+        double d = calculateDistance(points[i], points[i + 1]);
+        sum = sum + d;
     }
-    totalDistance += calculateDistance(tour[tour.size() - 1], tour[0]);
-    return totalDistance;
+    return sum;
 }
 
-void drawCities(sf::RenderWindow& window, const std::vector<City>& cities)
-{
-    sf::CircleShape cityShape(5.f);
-    cityShape.setFillColor(sf::Color::Red);
-
-    for (const auto& city : cities)
-    {
-        cityShape.setPosition(city.x, city.y);
-        window.draw(cityShape);
-    }
+void customSwap(std::vector<City>& cities, int i, int j) {
+    std::swap(cities[i], cities[j]);
 }
 
-void drawPath(sf::RenderWindow& window, const std::vector<City>& path)
-{
-    sf::VertexArray lines(sf::LineStrip);
-    lines.setPrimitiveType(sf::LinesStrip);
-    lines.resize(path.size());
-
-    for (std::size_t i = 0; i < path.size(); ++i)
-    {
-        lines[i].position = sf::Vector2f(path[i].x, path[i].y);
-        lines[i].color = sf::Color::Blue;
-    }
-
-    window.draw(lines);
-}
-
-std::vector<City> findShortestPath(const std::vector<City>& cities, sf::RenderWindow& window)
-{
-    std::vector<City> shortestPath;
-    double shortestDistance = std::numeric_limits<double>::max();
-    std::vector<City> currentPath = cities;
-
-    do {
-        double currentDistance = calculateTourDistance(currentPath);
-
-        if (currentDistance < shortestDistance)
-        {
-            shortestDistance = currentDistance;
-            shortestPath = currentPath;
-
-            // Clear window and redraw the current best path
-            window.clear(sf::Color::White);
-            drawCities(window, cities);
-            drawPath(window, shortestPath);
-            window.display();
-        }
-    } while (std::next_permutation(currentPath.begin() + 1, currentPath.end(),
-        [](const City& city1, const City& city2) {
-            return std::less<int>()(city1.x, city2.x);
-        }));
-
-    return shortestPath;
-}
 
 int main()
 {
+   
+
+
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
     sf::RenderWindow window(sf::VideoMode(Width, Height), "SFML IMGUI", sf::Style::Default, settings);
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(20);
     sf::Clock clock;
 
     ImGui::SFML::Init(window);
 
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-    bool show = true;
+    bool show = false;
 
     sf::Color red(235, 69, 95);
     sf::Color green(13, 84, 93);
@@ -128,13 +85,39 @@ int main()
     }
 
     std::vector<City> cities = {
-      {100, 100},
+      {100, 500},
       {200, 300},
       {400, 200},
       {600, 350},
-      {700, 150}
+      {500, 400},
+      {170, 250},
+      {120, 630},
     };
-    std::vector<City> shortestPath = findShortestPath(cities, window);
+
+    std::vector<sf::CircleShape> circles;
+
+    for (int i = 0; i < cities.size(); i++) {
+        sf::CircleShape circle(10);  // Create a circle with radius 50
+        circle.setPosition(cities[i].x - 10, cities[i].y - 10);  // Set position to (100, 100)
+        circle.setFillColor(sf::Color::White);  // Set fill color to red
+
+        circles.push_back(circle);
+
+    }
+
+    sf::RectangleShape lineSegment;
+
+
+    double recordDistance;
+    std::vector<City> bestEver;
+    double d = calcdist(cities);
+    recordDistance = d;
+    bestEver = cities;
+
+    std::random_device rd; // Create a random device to seed the generator
+    std::mt19937 generator(rd()); // Create the Mersenne Twister generator
+    std::uniform_int_distribution<int> distribution(0, cities.size() - 1); // Create a uniform distribution for indices
+
 
     while (window.isOpen())
     {
@@ -152,13 +135,57 @@ int main()
         ImGui::SFML::Update(window, clock.restart());
 
         window.clear();
-        drawCities(window, cities);
-        drawPath(window, shortestPath);
+      
         // Drawing the background grid
         window.draw(grid);
 
+       
         ImGui::SFML::Render(window);
+        int i = distribution(generator);
+        int j = distribution(generator);
+
+        // Make sure i and j are distinct
+        while (i == j) {
+            j = distribution(generator);
+        }
+        customSwap(cities, i, j);
+        d = calcdist(cities);
+
+
+        if (d < recordDistance) {
+            recordDistance = d;
+            bestEver = cities;
+            std::cout << " Record Distance is " << recordDistance << std::endl;
+            std::cout.flush();
+
+        }
+
+
+        for (int i = 0; i < cities.size(); i++ ) {
+            int nextIndex = (i + 1) % cities.size(); // Calculate the next city's index with wraparound
+            lineSegment.setSize(sf::Vector2f(calculateDistance(cities[i], cities[nextIndex]), 1.5f));
+            lineSegment.setFillColor(sf::Color::Green);
+            lineSegment.setPosition(sf::Vector2f(cities[i].x, cities[i].y));
+            lineSegment.setRotation(std::atan2(cities[nextIndex].y - cities[i].y, cities[nextIndex].x - cities[i].x) * 180.f / 3.14159265f);
+            window.draw(lineSegment);
+            window.draw(circles[i]);
+        }
+
+        for (int i = 0; i < bestEver.size(); i++) {
+            int nextIndexx = (i + 1) % bestEver.size(); // Calculate the next city's index with wraparound
+            lineSegment.setSize(sf::Vector2f(calculateDistance(bestEver[i], bestEver[nextIndexx]), 3.0f));
+            lineSegment.setFillColor(sf::Color::Blue);
+            lineSegment.setPosition(sf::Vector2f(bestEver[i].x, bestEver[i].y));
+            lineSegment.setRotation(std::atan2(bestEver[nextIndexx].y - bestEver[i].y, bestEver[nextIndexx].x - bestEver[i].x) * 180.f / 3.14159265f);
+            window.draw(lineSegment);
+
+        }
+        Overlay::ShowSideBar();
+        Overlay::ShowOverlay(&show);
+        ImGui::SFML::Render(window);
+
         window.display();
+
     }
 
     ImGui::SFML::Shutdown();
