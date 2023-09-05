73d4a207356992e6b75e905e90d90f2117c2c12a
Chadi Hamrouni <69485266+ChadiHamrouni@users.noreply.github.com>
2023-09-05 21:35:10 +0100

    Update Simulated Annealing.cpp

--- Simulated Annealing.cpp
+++ Simulated Annealing.cpp
@@ -1,14 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
-#include <algorithm>
 #include <vector>
 #include <iostream>
+#include <random>
 
-#include <cstdlib>
-#include <ctime>
-#include <random> // Include the random library
-
-int Width = 1280;  
+int Width = 1280;
 int Height = 720;
 
 const int NumRows = 14;
@@ -28,17 +24,14 @@ double calculateDistance(const City& city1, const City& city2)
     return std::sqrt(dx * dx + dy * dy);
 }
 
-double calcdist(std::vector<City>& points) {
-   
+double calcdist(const std::vector<City>& points)
+{
     double sum = 0;
-    for (int i = 0; i < points.size()-1; i++) {
+    for (int i = 0; i < points.size() - 1; i++) {
         double d = calculateDistance(points[i], points[i + 1]);
-        sum = sum + d;
+        sum += d;
     }
     return sum;
-}
-void customSwap(std::vector<City>& cities, int i, int j) {
-    std::swap(cities[i], cities[j]);
 }
 
 int main()
@@ -46,10 +39,9 @@ int main()
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode(Width, Height), "SFML IMGUI", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(Width, Height), "SFML TSP with Simulated Annealing", sf::Style::Default, settings);
     window.setFramerateLimit(120);
     sf::Clock clock;
-    bool show = false;
 
     sf::Color red(235, 69, 95);
     sf::Color green(13, 84, 93);
@@ -71,109 +63,85 @@ int main()
     }
 
     std::vector<City> cities = {
-      {100, 500},
-      {200, 300},
-      {400, 200},
-      {600, 350},
-      {500, 400},
-      {170, 250},
-      {120, 630},
+        {100, 500},
+        {200, 300},
+        {400, 200},
+        {600, 350},
+        {500, 400},
+        {170, 250},
+        {120, 630},
     };
 
     std::vector<sf::CircleShape> circles;
 
-    for (int i = 0; i < cities.size(); i++) {
-        sf::CircleShape circle(10);  // Create a circle with radius 50
-        circle.setPosition(cities[i].x - 10, cities[i].y - 10);  // Set position to (100, 100)
-        circle.setFillColor(sf::Color::White);  // Set fill color to red
+    for (const auto& city : cities) {
+        sf::CircleShape circle(10);
+        circle.setPosition(city.x - 10, city.y - 10);
+        circle.setFillColor(sf::Color::White);
         circles.push_back(circle);
-
     }
 
     sf::RectangleShape lineSegment;
 
-    double recordDistance;
-    std::vector<City> bestEver;
-    double d = calcdist(cities);
-    recordDistance = d;
-    bestEver = cities;
+    double recordDistance = calcdist(cities);
+    std::vector<City> bestEver = cities;
 
-    std::random_device rd; // Create a random device to seed the generator
-    std::mt19937 generator(rd()); // Create the Mersenne Twister generator
-    std::uniform_int_distribution<int> distribution(0, cities.size() - 1); // Create a uniform distribution for indices
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> distribution(0, cities.size() - 1);
 
-    int i = distribution(generator); // Initialize i and j outside the loop
-    int j = distribution(generator); // Initialize i and j outside the loop
-
-    std::random_shuffle(cities.begin(), cities.end());
-
-    double initialTemperature = 1000;  // Adjust the initial temperature
-    double coolingRate = 0.995; // Cooling rate
+    double initialTemperature = 1000.0;
+    double coolingRate = 0.995;
     int maxIterations = 50000;
-    int maxNonImprovingIterations = 5000; // Set a limit for non-improving iterations
 
-
-    double temperature = initialTemperature;
-    int nonImprovingCount = 0;
-
-    while (window.isOpen() && maxIterations > 0 && nonImprovingCount < maxNonImprovingIterations)
+    while (window.isOpen() && maxIterations > 0)
     {
-        sf::Time elapsed = clock.restart(); // get the time elapsed since the last restart and restart the clock
+        sf::Time elapsed = clock.restart();
         float dt = elapsed.asSeconds();
 
         sf::Event event;
         while (window.pollEvent(event))
         {
-           
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        window.clear();
+        window.draw(grid);
+
+        // Store a copy of the current solution for potential reversion
+        std::vector<City> currentSolution = cities;
+
+        // Randomly select two indices for swapping
+        int i = distribution(generator);
+        int j = distribution(generator);
+
         // Make sure i and j are distinct
         while (i == j) {
-            i = distribution(generator);
-
             j = distribution(generator);
         }
 
-        customSwap(cities, i, j);
-        // Drawing the background grid
-        double newDistance = calcdist(cities);
-        double delta = newDistance - d;
+        // Swap the cities in the current solution
+        std::swap(currentSolution[i], currentSolution[j]);
 
-        if (delta < 0 || (rand() / (RAND_MAX + 1.0)) < exp(-delta / temperature)) {
-            d = newDistance;
-            bestEver = cities;
-            if (d < recordDistance) {
-                recordDistance = d;
-                nonImprovingCount = 0; // Reset non-improving count
+        // Calculate distances for the current and best solutions
+        double currentDistance = calcdist(currentSolution);
 
+        // Calculate the change in distance (delta)
+        double delta = currentDistance - calcdist(cities);
+
+        // Check if the new solution is accepted based on a probabilistic criterion
+        if (delta < 0 || (rand() / (RAND_MAX + 1.0)) < exp(-delta / initialTemperature)) {
+            cities = currentSolution;  // Accept the new solution
+            if (currentDistance < recordDistance) {
+                recordDistance = currentDistance;
+                bestEver = cities;
                 std::cout << "Record Distance is " << recordDistance << std::endl;
-                std::cout.flush();
             }
-        } else {
-            // Revert the swap if the solution is rejected
-            customSwap(cities, i, j);
-            nonImprovingCount++; // Increment non-improving count
-
         }
 
-       
-        temperature *= coolingRate;
-        if (temperature < 1.0) {
-            // You can add additional logic to reset the solution when temperature gets low enough
-            // For example, shuffle the cities and start again
-            std::random_shuffle(cities.begin(), cities.end());
-            d = calcdist(cities);
-            nonImprovingCount = 0; // Reset non-improving count
-            temperature = initialTemperature; // Reset temperature
-        }
-        window.clear();
-
-       
-        window.draw(grid);
-
-        for (int i = 0; i < cities.size(); i++ ) {
+        // Draw the tour for the current solution
+        for (int i = 0; i < cities.size(); i++) {
             int nextIndex = (i + 1) % cities.size();
             lineSegment.setSize(sf::Vector2f(calculateDistance(cities[i], cities[nextIndex]), 1.5f));
             lineSegment.setFillColor(sf::Color::Green);
@@ -183,20 +151,23 @@ int main()
             window.draw(circles[i]);
         }
 
-        for (int j = 0; j < bestEver.size(); j++) {
-            int nextIndexx = (j + 1) % bestEver.size(); // Calculate the next city's index with wraparound
-            lineSegment.setSize(sf::Vector2f(calculateDistance(bestEver[j], bestEver[nextIndexx]), 3.0f));
+        // Draw the tour for the best solution found so far
+        for (int i = 0; i < bestEver.size(); i++) {
+            int nextIndexx = (i + 1) % bestEver.size();
+            lineSegment.setSize(sf::Vector2f(calculateDistance(bestEver[i], bestEver[nextIndexx]), 3.0f));
             lineSegment.setFillColor(sf::Color::Blue);
-            lineSegment.setPosition(sf::Vector2f(bestEver[j].x, bestEver[j].y));
-            lineSegment.setRotation(std::atan2(bestEver[nextIndexx].y - bestEver[j].y, bestEver[nextIndexx].x - bestEver[j].x) * 180.f / 3.14159265f);
+            lineSegment.setPosition(sf::Vector2f(bestEver[i].x, bestEver[i].y));
+            lineSegment.setRotation(std::atan2(bestEver[nextIndexx].y - bestEver[i].y, bestEver[nextIndexx].x - bestEver[i].x) * 180.f / 3.14159265f);
             window.draw(lineSegment);
-
         }
-     
+
         window.display();
         maxIterations--;
-       
+
+        // Reduce the initial temperature
+        initialTemperature *= coolingRate;
     }
 
     return 0;
 }
+
